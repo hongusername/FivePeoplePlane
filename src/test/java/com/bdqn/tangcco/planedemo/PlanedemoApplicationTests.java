0f423c7903e29c2e19cb46ddd5fd21244451d71f
lenovo <1847268563@qq.com>
2019-01-12 14:46:52 +0800

    昔哥管理员表写完了，不用谢！

--- src/test/java/com/bdqn/tangcco/planedemo/PlanedemoApplicationTests.java
+++ src/test/java/com/bdqn/tangcco/planedemo/PlanedemoApplicationTests.java
@@ -1,16 +1,62 @@
 package com.bdqn.tangcco.planedemo;
 
+import com.bdqn.tangcco.planedemo.admin.adminService.AdminService;
+import com.bdqn.tangcco.planedemo.entity.Admin;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PlanedemoApplicationTests {
+    @Resource
+    private DataSource dataSource;
+    @Resource
+    private AdminService adminService;
 
     @Test
     public void contextLoads() {
+        try {
+            Connection connection = dataSource.getConnection();
+            System.out.println(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //Admin
+    @Test
+    public void testQuery() {
+        System.out.println(adminService.queryAdmin());
+    }
+
+    @Test
+    public void testaddAdmin() {
+        System.out.println(adminService.addAdmin(new Admin(5, 2, "Whit?", 1234123132, "adaddsss", null, 2)));
+    }
+
+    @Test
+    public void testQueryById() {
+        System.out.println(adminService.queryByAdminId(2));
+    }
+
+    @Test
+    public void testdelete() {
+        System.out.println(adminService.deleteAdmin(4));
+    }
+    @Test
+    public void testupdate(){
+        /*SimpleDateFormat sm=new SimpleDateFormat("yyyy-MM-dd");
+        String shijian="2018-12-12";
+        Date shijian2=(new SimpleDateFormat("yyyy-MM-dd")).format(shijain);*/
+        System.out.println(adminService.updateAdmin(new Admin(5,0,"OK",0,"ok",null,0)));
     }
 
 }
