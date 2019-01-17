b6be236ef6c6eff43814f50797fed4a6c882ccc1
ShiDunKai <1658554876@qq.com>
2019-01-17 13:44:16 +0800

    留言表完成

---
+++ src/main/java/com/bdqn/tangcco/planedemo/entity/Catalog.java
@@ -0,0 +1,16 @@
+package com.bdqn.tangcco.planedemo.entity;
+
+/**
+ * @program: planedemo
+ * @description: ${description}
+ * @author: ShiDunKai
+ * @create: 2019-01-15 16:58
+ **/
+public class Catalog {
+    private Integer catalogId;
+    private String cataLaws;
+    private String cataText;
+    private Integer cateGrade;
+    private Integer cateRank;
+    private Integer cateNumber;
+}
---
+++ src/main/java/com/bdqn/tangcco/planedemo/entity/Laws.java
@@ -0,0 +1,19 @@
+package com.bdqn.tangcco.planedemo.entity;
+
+/**
+ * @program: planedemo
+ * @description: ${description}
+ * @author: ShiDunKai
+ * @create: 2019-01-15 17:00
+ **/
+public class Laws {
+    private String lawsId;
+    private Integer lawsPart;
+    private String lawsVersion;
+    private Integer lawsStatus;
+    private String lawsName;
+    private String lawsDownload;
+    private String lawsState;
+    private String lawsLogo;
+    private String lawsFDBName;
+}
