f81d835e0a234e9a239a58b69d15828e1f59c1bb
chenjianghong <2403168915@qq.com>
2019-01-17 17:36:12 +0800

    一对一一萨达萨达萨达是

--- src/main/java/com/bdqn/tangcco/planedemo/entity/Catalog.java
+++ src/main/java/com/bdqn/tangcco/planedemo/entity/Catalog.java
@@ -1,11 +1,18 @@
 package com.bdqn.tangcco.planedemo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 /**
  * @program: planedemo
  * @description: ${description}
  * @author: ShiDunKai
  * @create: 2019-01-15 16:58
  **/
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Data
 public class Catalog {
     private Integer catalogId;
     private String cataLaws;
